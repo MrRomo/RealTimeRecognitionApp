4cefdfd611f3109253b3e697d23bf61205f3f121
dilansolart <44990926+dilansolart@users.noreply.github.com>
2019-09-22 00:30:52 -0500

    Update t2s.py

--- t2s.py
+++ t2s.py
@@ -2,9 +2,12 @@ from gtts import gTTS
 import os
 
 file = "file.mp3"
-tts = gTTS('Aura. Me alegro que estes viendo peliculas. . . Yo estoy aqui programando', lang='es-us', slow=False)
+
+tts1 = gTTS('Insertar caracteristica 1', lang='es-us', slow=False)
+tts2 = gTTS('Insertar caracteristica 2', lang='es-us', slow=False)
 
 with open(file, "wb") as archivo:
-    tts.write_to_fp(archivo)
+    tts1.write_to_fp(archivo)
+    tts2.write_to_fp(archivo)
 
 os.system("mpg123 " + file)
